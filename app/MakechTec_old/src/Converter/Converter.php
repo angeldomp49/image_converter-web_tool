2b6aa5297e1b580de903bda76a3c168b2450c066
Makech Tecnology <contacto@makechtecnology.com>
2021-11-17 21:07:22 -0600

    makechtec

--- app/MakechTec_old/src/Converter/Converter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace MakechTec\ImageConverter\Converter;
-
-use MakechTec\ImageConverter\Img\{ImgFile, ImgContainer};
-
-abstract class Converter{
-    public abstract function convert( ImgFile $imgFile, String $pathName );
-    public abstract function convertAll( ImgContainer $fileContainer, String $destinationDirectory );
-}
